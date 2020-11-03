faf14acd78155011c5a341f853c44d37935ca03d
abid7 <abidshahriar7@gmail.com>
2020-11-04 00:22:19 +0600

    clas- 4/11/2020

--- index.js
+++ index.js
@@ -1,55 +1,69 @@
-// const form = document.querySelector("form");
+// string and string methods*******************
 
-// form.addEventListener("submit", function (e) {
-//   event.preventDefault();
+// const firstName = "Abid";
+// const lastName = "Shahriar";
 
-// });
+// const fullName = "my name is: " + firstName + " " + lastName; //old way of concate**
+// const fullName = `my name is: ${firstName} ${lastName}`;   //new way of concate**
 
-// const num1 = 4;
+// console.log(fullName);
 
-// if (num1 > 5) {
-//   console.log("the number is 5");
-// } else {
-//   console.log("the number is not five");
-// }
+// const ob1 = {
+//   name: "abid",
+//   birthYear: 1997,
 
-// const btn = document.querySelector("button");
-
-// let darkmode = true;
-
-// btn.addEventListener("click", function () {
-//   if (darkmode === true) {
-//     darkmode = false;
-//   } else if (darkmode === false) {
-//     darkmode = true;
-//   }
-
-//   if (darkmode === true) {
-//     document.body.style.backgroundColor = "white";
-//   } else {
-//     document.body.style.backgroundColor = "black";
-//   }
-
-//   console.log(darkmode);
-// });
-
-// const div = document.querySelector(".ourDiv");
-// console.log(div);
-
-// const btn = document.querySelector("button");
-
-// btn.addEventListener("click", function () {
-//   div.classList.add("black-bg");
-// });
-
-// 100 <= 20%
-// 100-200 15%
-// 200 10%
-
-// let friend = ["abid", "sajjad", "shohag"];
-
-// let family = {
-//   father: "abul kalam",
-//   mother: "arjuman",
-//   brother: "abid",
+//   age: function () {
+//     let year = 2020;
+//     return year - this.birthYear;
+//   },
 // };
+
+// console.log(ob1.age());
+
+// const fullName = "Abid Shahriar";
+
+// const result = fullName.length;
+// const result = fullName.slice("2", "8");
+// const result = fullName.substr("2", "9");
+// const result = fullName.replace("i", "Z");
+
+// console.log(result);
+
+// tip calculator app*******************
+// const input = document.querySelector(".input");
+// const btn = document.querySelector(".btn");
+// const totalBill = document.querySelector(".total-bill");
+
+// btn.addEventListener("click", function () {
+//   let billString = input.value;
+//   let bill = Number(billString);
+//   if (bill > 0 && bill <= 100) {
+//     let tip = bill * (20 / 100);
+//     let fullBill = bill + tip;
+//     totalBill.innerHTML = fullBill;
+//   } else if (bill > 100 && bill <= 200) {
+//     let tip = bill * (15 / 100);
+//     let fullBill = bill + tip;
+//     totalBill.innerHTML = fullBill;
+//   } else if (bill > 200) {
+//     let tip = bill * (10 / 100);
+//     let fullBill = bill + tip;
+//     totalBill.innerHTML = fullBill;
+//   }
+// });
+
+// homework *********************************
+// 200
+// your total bill is 230 taka with 15% tip
+
+// more about object and array**********************************
+// let arr = [123, "abid", true, ["abid", "akash"], "hello", "hi"];
+
+// console.log(arr[3][1]);
+
+// const ob2 = {
+//   friend: ["abid", "akash", "rahim"],
+//   family: ["father", "motherr", "sister"],
+// };
+
+// console.log(ob2.friend[2]);
